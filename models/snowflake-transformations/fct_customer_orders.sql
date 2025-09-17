b93da1351e940f47bb95e2d474ecba4d506d7f42
martindqlabs <martin@dqlabs.ai>
2025-09-17 18:45:27 +0530

    Update fct_customer_orders.sql

--- models/snowflake-transformations/fct_customer_orders.sql
+++ models/snowflake-transformations/fct_customer_orders.sql
@@ -11,7 +11,6 @@ SELECT
     order_status,
     total_amount,
     payment_id,
-    payment_date,
     quantity * price_per_item AS calculated_total_item_amount,
     CASE 
         WHEN total_amount != quantity * price_per_item THEN 'Mismatch' 
